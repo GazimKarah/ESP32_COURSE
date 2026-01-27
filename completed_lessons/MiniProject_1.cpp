ae8650ea038eae3f75189a3a194d2b9a98cc6228
GazimKarah <GazimKarah@users.noreply.github.com>
2026-01-28 02:57:07 +0300

    mini project repair

--- completed_lessons/MiniProject_1.cpp
+++ completed_lessons/MiniProject_1.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 
 #define LED_PIN 2
+#define interrupt_PIN 18 // GPIO pin for the button
+uint8_t led_status =0;
 
 unsigned long Time = 0 ;
 unsigned long WaitingTime = 1000;
@@ -10,54 +12,25 @@ int lineIndex = 0;
 
 void setup() {
 
-  pinMode(LED_PIN, OUTPUT);
-  
   Serial.begin(115200);
-  Serial.println("\n Serial Initialized and LED Period set to 1000 ms");
+  pinMode (LED_PIN, OUTPUT);
+  pinMode (interrupt_PIN, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt (interrupt_PIN), blink_and_count, FALLING);
+ 
 
 }
 void loop() {
  
-  char c;
-  unsigned long CurrentTime = millis();
+  while(1){
+    // Do nothing, everything is handled in the interrupt
+  }
+}
 
+void blink_and_count(){
 
-    if (Serial.available() != 0){
-      c = Serial.read();
-      
-      if (c == '\n'){
-        Serial.println(); // Echo newline
-        lineBuffer[lineIndex] = '\0';
-        if(strncmp(lineBuffer, "period " , 7) ==0){
-          WaitingTime = atoi(&lineBuffer[7]);
-          Serial.println("Period set to " + String(WaitingTime) + " ms");
-        }
-        else {
-          Serial.println("Unknown Command");
-        }
-        lineIndex = 0;
-      }
-
-      else if (c == '\b' || c == 127){
-        if(lineIndex > 0){
-          lineIndex--;
-          lineBuffer[lineIndex] = '\0';  // Null-terminate the string that means the phrase has came to an end
-          Serial.print("\b \b"); // Move cursor back, print space, move cursor back again
-        }
-      }
-      else if (c != '\r'){
-        Serial.write(c); // Echo back the received character
-        
-        lineBuffer[lineIndex] = c;
-        lineIndex++;
-        if(lineIndex >= sizeof(lineBuffer) - 1){
-          lineIndex = sizeof(lineBuffer) - 2;
-        }
-
-      }
-    }
-    if (CurrentTime - Time >=  WaitingTime){
-      Time = CurrentTime;
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    }
+  static int count = 0;
+  digitalWrite(LED_PIN, !led_status); 
+  led_status = !led_status;
+  count++;
+  Serial.println(count);  
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,159 +1,35 @@
 #include <Arduino.h>
 
 #define LED_PIN 2
+#define interrupt_PIN 18 // GPIO pin for the button
+volatile bool toogleReq = false;
+volatile uint32_t count =0;
 
-unsigned long Time = 0 ;
-unsigned long WaitingTime = 1000;
 
-char lineBuffer[32];
-int lineIndex = 0;
-bool overflowed = false;
-bool handled = false;
-
-unsigned long parseMs(const char* s, bool* ok);
-bool AllDigits(const char* s);
+void IRAM_ATTR blink_and_count();
 
 void setup() {
 
-  pinMode(LED_PIN, OUTPUT);
-  
   Serial.begin(115200);
-  Serial.println("\n Serial Initialized and LED Period set to 1000 ms");
+  Serial.println("Interrupt Example Started");
+  pinMode (LED_PIN, OUTPUT);
+  pinMode (interrupt_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt (interrupt_PIN), blink_and_count , FALLING);
+ 
 
 }
-
 void loop() {
  
-  char c;
-  unsigned long CurrentTime = millis();
-
-      if (CurrentTime - Time >= WaitingTime){
-
-      Time = CurrentTime;
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      }
-
-  while (Serial.available() != 0) {
-
-    c = Serial.read();
-
-    // Handle input overflow
-    if (overflowed == true) {
-
-        
-        if(c == '\n') {
-
-            overflowed = false;
-            lineIndex = 0;
-            lineBuffer[0] = '\0';
-            Serial.println();
-            Serial.println("ERR: Input Overflow.");
-            continue;
-
-        }
-        continue; // Discard characters until newline is received
+    if (toogleReq){
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN)); // Toggle LED state
+      Serial.print("Button pressed ");
+      Serial.print(count);
+      Serial.println(" times");
+      toogleReq = false;
     }
-    if (c == '\b' || c == 127){
-
-        if(lineIndex > 0){ 
-            lineIndex--;
-            lineBuffer[lineIndex] = '\0';  // Null-terminate the string that means the phrase has came to an end
-            Serial.print("\b \b"); // Move cursor back, print space, move cursor back again
-        }
-        continue; // Ignore backspace if at the start of the line
-    }
-
-    if (c == '\r') {
-    continue;
-    }
-    else {
-
-        // Check for newline character to process the command
-        if (c == '\n') {
-
-            handled = false;
-            Serial.println(); // Echo newline
-            lineBuffer[lineIndex] = '\0';
-
-            //Command Parsing
-            if(strncmp(lineBuffer, "period" , 6) ==0){
-
-            // Is "period" command
-                if(lineBuffer[6] == '\0' || lineBuffer[6] == ' '){
-                    char* arg = &lineBuffer[6]; // Pointer to the argument part
-                while (*arg == ' ') arg++; // Skip spaces
-                   
-                if(*arg == '\0'){
-                    Serial.println("ERR: Missing Number Argument.");
-                    handled = true;
-                }
-                else if(!AllDigits(arg)){
-
-                    Serial.println("ERR: Invalid Number Argument.");
-                    handled = true;
-                }
-                else {
-                    bool ok = false;
-
-                    unsigned long period = parseMs(arg, &ok);
-
-                    const unsigned long MIN_PERIOD = 10; // 10 ms
-                    const unsigned long MAX_PERIOD = 60000; // 60 seconds
-
-                    if(!ok){
-                        Serial.println("ERR: Invalid Number Argument.");
-                    }
-                    else if (period < MIN_PERIOD || period > MAX_PERIOD){
-                        Serial.println("ERR: Period Out Of Range. Must be between " + String(MIN_PERIOD) + " ms and " + String(MAX_PERIOD) + " ms.");
-                    }
-                    else{
-                        WaitingTime = period;
-                        Serial.println("Period set to " + String(WaitingTime) + " ms");
-                    }
-                    handled = true;
-                    }
-                }
-            }
-
-            if (handled == false && lineIndex > 0){
-                Serial.println("Unknown Command");
-            }
-
-                    lineIndex = 0;
-                    lineBuffer[0] = '\0';
-                    continue;
-        }
-
-                // Command was handled successfully
-                if(lineIndex < (int)sizeof(lineBuffer) - 1){
-                    lineBuffer[lineIndex++] = c;
-                    lineBuffer[lineIndex] = '\0'; // Null-terminate the string
-                    Serial.write(c); // Echo back the received character
-
-                }
-                else {
-                    overflowed = true;
-                }
-
-    }
-  }
 }
 
-
-unsigned long parseMs(const char* s, bool* ok){ // Parses a string to extract an unsigned long representing milliseconds
-
-    char* endptr = nullptr; // Pointer to track where parsing stopped
-    unsigned long value = strtoul(s, &endptr, 10); // Convert string to unsigned long
-    *ok = (endptr != s) && (*endptr == '\0'); // Check if parsing was successful and consumed the entire string
-    return value;
+void IRAM_ATTR blink_and_count(){
+  count++;
+  toogleReq = !toogleReq; 
 }
-
-bool AllDigits(const char* s){
-    if (*s == '\0') return false; // Empty string check
-    while (*s) {
-        if (*s < '0' || *s > '9') return false; // Non-digit character found (We check ASCII values here ('0' = 48, '9' = 57)  and if it is outside this range, it is not a digit)
-        s++; // Move to the next character (\0 is essential for the loop termination if we do not place it end of the word, it will continue reading garbage values in memory))
-        
-    }
-    return true; // All characters are digits
-    }
